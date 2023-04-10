b857da6cac20817dcc46be4a03e916842503c521
CorruptionHades <96599430+CorruptionHades@users.noreply.github.com>
2023-04-10 17:04:50 +0200

    Arraylist update
    works with movin around

--- src/main/java/me/stormcph/lumina/module/impl/render/Arraylist.java
+++ src/main/java/me/stormcph/lumina/module/impl/render/Arraylist.java
@@ -6,6 +6,7 @@ import me.stormcph.lumina.module.Module;
 import me.stormcph.lumina.module.ModuleManager;
 import net.minecraft.client.util.math.MatrixStack;
 
+import java.util.Comparator;
 import java.util.List;
 
 public class Arraylist extends HudModule {
@@ -16,19 +17,43 @@ public class Arraylist extends HudModule {
 
     @Override
     public void draw(MatrixStack matrices) {
-        int index = 0;
-        int sWidth = mc.getWindow().getScaledWidth();
-        int sHeight = mc.getWindow().getScaledHeight();
 
         // TODO: coordinate usage & set width/height
 
+        boolean topLeft = getX() < (mc.getWindow().getScaledWidth() / 2) && getY() < (mc.getWindow().getScaledHeight() / 2);
+        boolean topRight = getX() > (mc.getWindow().getScaledWidth() / 2) && getY() < (mc.getWindow().getScaledHeight() / 2);
+        boolean bottomLeft = getX() < (mc.getWindow().getScaledWidth() / 2) && getY() > (mc.getWindow().getScaledHeight() / 2);
+        boolean bottomRight = getX() > (mc.getWindow().getScaledWidth() / 2) && getY() > (mc.getWindow().getScaledHeight() / 2);
+
         List<Module> enabled = ModuleManager.INSTANCE.getEnabledModules();
 
-        enabled.sort((m1, m2) -> (int) (mc.textRenderer.getWidth(m2.getName() + m2.getDisplayName()) - mc.textRenderer.getWidth(m1.getName() + m1.getDisplayName())));
+        if(topLeft || topRight) enabled.sort((m1, m2) -> mc.textRenderer.getWidth(m2.getDisplayName()) - mc.textRenderer.getWidth(m1.getDisplayName()));
+        else if(bottomLeft || bottomRight) enabled.sort(Comparator.comparingInt(m -> mc.textRenderer.getWidth(m.getDisplayName())));
 
+        int width = 0;
+        int offset = 0;
         for (Module mod : enabled) {
-            mc.textRenderer.drawWithShadow(matrices, mod.getDisplayName(), (sWidth - 4) - mc.textRenderer.getWidth(mod.getDisplayName()), 10 + (index * mc.textRenderer.fontHeight), -1);
-            index++;
+
+            if(mc.textRenderer.getWidth(mod.getDisplayName()) > width) {
+                width = mc.textRenderer.getWidth(mod.getDisplayName());
+            }
+
+            if(topLeft) {
+                mc.textRenderer.drawWithShadow(matrices, mod.getDisplayName(), getX(), getY() + offset, -1);
+            }
+            else if(topRight) {
+                mc.textRenderer.drawWithShadow(matrices, mod.getDisplayName(), getX() + getWidth() - mc.textRenderer.getWidth(mod.getDisplayName()), getY() + offset, -1);
+            }
+            else if(bottomLeft) {
+                mc.textRenderer.drawWithShadow(matrices, mod.getDisplayName(), getX(), getY() + offset, -1);
+            }
+            else if(bottomRight) {
+                mc.textRenderer.drawWithShadow(matrices, mod.getDisplayName(), getX() + getWidth() - mc.textRenderer.getWidth(mod.getDisplayName()), getY() + offset, -1);
+            }
+            offset += mc.textRenderer.fontHeight + 2;
         }
+
+        setHeight(offset);
+        setWidth(width);
     }
 }
