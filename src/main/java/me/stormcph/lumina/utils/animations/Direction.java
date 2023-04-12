e5e3590f1b3bd4ca585b67c096df3ba3bdd894af
CorruptionHades <96599430+CorruptionHades@users.noreply.github.com>
2023-04-12 20:34:19 +0200

    Add files via upload

---
+++ src/main/java/me/stormcph/lumina/utils/animations/Direction.java
@@ -0,0 +1,22 @@
+package me.stormcph.lumina.utils.animations;
+
+public enum Direction {
+    FORWARDS,
+    BACKWARDS;
+
+    public Direction opposite() {
+        if (this == Direction.FORWARDS) {
+            return Direction.BACKWARDS;
+        } else return Direction.FORWARDS;
+    }
+
+
+    public boolean forwards() {
+        return this == Direction.FORWARDS;
+    }
+
+    public boolean backwards() {
+        return this == Direction.BACKWARDS;
+    }
+
+}
