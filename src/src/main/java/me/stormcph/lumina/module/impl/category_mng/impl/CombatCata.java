1e560d5a5d0d9fb085f34c10704409fad233029b
stormcph <122960804+LuminaDevelopment@users.noreply.github.com>
2023-04-14 07:42:15 +0200

    [-] src placed wrong

--- src/src/main/java/me/stormcph/lumina/module/impl/category_mng/impl/CombatCata.java
+++
@@ -1,9 +0,0 @@
-package me.stormcph.lumina.module.impl.category_mng.impl;
-
-import me.stormcph.lumina.module.impl.category_mng.CategoryModule;
-
-public class CombatCata extends CategoryModule {
-    public CombatCata() {
-        super("Combat");
-    }
-}
--- src/src/main/java/me/stormcph/lumina/module/impl/category_mng/impl/PlayerCategory.java
+++
@@ -1,9 +0,0 @@
-package me.stormcph.lumina.module.impl.category_mng.impl;
-
-import me.stormcph.lumina.module.impl.category_mng.CategoryModule;
-
-public class PlayerCategory extends CategoryModule {
-    public PlayerCategory() {
-        super("Player");
-    }
-}
