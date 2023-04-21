85059f5098bcb89ca2fbf4732493149e116134a1
stormcph <mr.capyyy@gmail.com>
2023-04-21 19:11:58 +0200

    [+] scale option to LuminaLogo module

--- src/main/java/me/stormcph/lumina/module/impl/render/LuminaLogo.java
+++ src/main/java/me/stormcph/lumina/module/impl/render/LuminaLogo.java
@@ -2,8 +2,7 @@ package me.stormcph.lumina.module.impl.render;
 
 import me.stormcph.lumina.module.Category;
 import me.stormcph.lumina.module.HudModule;
-import me.stormcph.lumina.setting.impl.BooleanSetting;
-import me.stormcph.lumina.utils.GifTool;
+import me.stormcph.lumina.setting.impl.NumberSetting;
 import me.stormcph.lumina.utils.RenderUtils;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.util.math.MatrixStack;
@@ -16,14 +15,18 @@ public class LuminaLogo extends HudModule {
     private int index = 0;
     private int counter = 0;
 
+    private NumberSetting scaleSetting;
+
     public LuminaLogo() {
-        super("LuminaLogo", "The Best Module", Category.RENDER, 16, 16, 16, 16);
+        super("LuminaLogo", "The Best Module", Category.RENDER, 256, 256, 256, 256);
+        scaleSetting = new NumberSetting("Scale", 0.08, 2.0, 0.35, 0.001);
+        this.addSettings(scaleSetting);
     }
 
     @Override
     public void onEnable() {
         super.onEnable();
-        frames = new String[]{"logo.png"};
+        frames = new String[]{"logo_256x.png"};
 
         counter = 0;
         index = 0;
@@ -34,24 +37,18 @@ public class LuminaLogo extends HudModule {
     public void draw(MatrixStack matrices) {
         if (frames == null) return;
 
-        setWidth(16);
-        setHeight(16);
+        float scale = (float) scaleSetting.getValue();
+        int width = (int) (scale * 256);
+        int height = (int) (scale * 256);
+
+        setWidth(width);
+        setHeight(height);
 
         counter++;
-        if(counter >= 8) {
-            if(index >= frames.length - 1) {
-                index = 0;
-            } else {
-                index++;
-            }
 
-            counter = 0;
-        }
-
-        float scale = 0.35f;
         matrices.push();
         matrices.scale(scale, scale, 0);
-        RenderUtils.drawTexturedRectangle(matrices, getX() * (1 /scale), getY() * (1 /scale), "textures/gui/" + frames[index]);
+        RenderUtils.drawTexturedRectangle(matrices, getX() * (1 / scale), getY() * (1 / scale), "textures/gui/" + frames[index]);
         matrices.pop();
     }
 }
