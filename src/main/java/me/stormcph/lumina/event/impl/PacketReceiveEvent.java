1ece6b86cbe64747a42b0cfc675c44d68a8d692e
stormcph <mr.capyyy@gmail.com>
2023-05-07 21:55:38 +0200

    [+] added none working packet settings XD

---
+++ src/main/java/me/stormcph/lumina/event/impl/PacketReceiveEvent.java
@@ -0,0 +1,21 @@
+package me.stormcph.lumina.event.impl;
+
+import me.stormcph.lumina.event.Event;
+import net.minecraft.network.packet.Packet;
+
+public class PacketReceiveEvent extends Event {
+
+    private Packet<?> packet;
+
+    public PacketReceiveEvent(Packet<?> packet) {
+        this.packet = packet;
+    }
+
+    public Packet<?> getPacket() {
+        return packet;
+    }
+
+    public void setPacket(Packet<?> packet) {
+        this.packet = packet;
+    }
+}
