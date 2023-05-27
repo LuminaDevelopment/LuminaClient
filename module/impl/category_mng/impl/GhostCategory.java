7c509f77db5a211725b48d15b4886d4427f10424
sassan <125366201+emirsassan@users.noreply.github.com>
2023-05-27 20:34:39 +0300

    Delete module directory

--- module/impl/category_mng/impl/GhostCategory.java
+++
@@ -1,9 +0,0 @@
-package me.stormcph.lumina.module.impl.category_mng.impl;
-
-import me.stormcph.lumina.module.impl.category_mng.CategoryModule;
-
-public class GhostCategory extends CategoryModule {
-    public GhostCategory() {
-        super("Ghost");
-    }
-}
