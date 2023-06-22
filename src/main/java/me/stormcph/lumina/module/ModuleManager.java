ae059031e125aead8ceba9af6dd9f4a8c633bb04
stormcph <122960804+LuminaDevelopment@users.noreply.github.com>
2023-06-22 20:45:24 +0200

    fixed mistage in ModuleManager

--- src/main/java/me/stormcph/lumina/module/ModuleManager.java
+++ src/main/java/me/stormcph/lumina/module/ModuleManager.java
@@ -52,7 +52,6 @@ public class ModuleManager {
         add(new Advertise());
 //        add(new AutoEz()); it's not finished yet cause i havent implemented a way to detect that it was actually you that killed the entity
         add(new NoTrace());
-        add(new ExternalMap());
 
         // Player
         add(new AutoArmor()); //blatant
