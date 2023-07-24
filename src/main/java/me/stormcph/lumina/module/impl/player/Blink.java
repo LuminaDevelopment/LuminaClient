2c95e139a606bf319e767c2b6a6fa6d16d152cd4
CorruptionHades <corruptionhades@gmail.com>
2023-07-24 18:47:28 +0200

    [+] Blink, Ghostplace, cleanup

---
+++ src/main/java/me/stormcph/lumina/module/impl/player/Blink.java
@@ -0,0 +1,70 @@
+package me.stormcph.lumina.module.impl.player;
+
+import me.stormcph.lumina.event.EventTarget;
+import me.stormcph.lumina.event.impl.PacketSendEvent;
+import me.stormcph.lumina.module.Category;
+import me.stormcph.lumina.module.Module;
+import me.stormcph.lumina.utils.player.PacketUtil;
+import me.stormcph.lumina.utils.render.FakePlayerEntity;
+import net.minecraft.item.BlockItem;
+import net.minecraft.item.ItemStack;
+import net.minecraft.network.packet.Packet;
+import net.minecraft.network.packet.c2s.play.KeepAliveC2SPacket;
+import net.minecraft.network.packet.c2s.play.PlayerInteractBlockC2SPacket;
+import net.minecraft.network.packet.c2s.query.QueryPingC2SPacket;
+
+import java.util.Queue;
+import java.util.concurrent.ConcurrentLinkedQueue;
+
+public class Blink extends Module {
+
+    private final Queue<Packet<?>> packets = new ConcurrentLinkedQueue<>();
+    private FakePlayerEntity model;
+
+    public Blink() {
+        super("Blink", "Cancels all packets", Category.PLAYER);
+    }
+
+    @Override
+    public void onEnable() {
+        super.onEnable();
+
+        model = new FakePlayerEntity(mc.player, mc.player.getGameProfile().getName(), 20, true);
+        model.doNotPush = true;
+        model.hideWhenInsideCamera = true;
+        model.spawn();
+    }
+
+    @EventTarget
+    public void onPacketSend(PacketSendEvent e) {
+        Packet<?> packet = e.getPacket();
+        if (packet instanceof KeepAliveC2SPacket || packet instanceof QueryPingC2SPacket) {
+            return;
+        }
+        else if(packet instanceof PlayerInteractBlockC2SPacket pib) {
+            ItemStack item = mc.player.getStackInHand(pib.getHand());
+            if(item != null) {
+                int count = item.getCount();
+                if (item.getItem() instanceof BlockItem) {
+                    mc.player.getStackInHand(pib.getHand()).setCount(count + 1);
+                }
+            }
+        }
+        e.cancel();
+        packets.add(packet);
+    }
+
+    @Override
+    public void onDisable() {
+        super.onDisable();
+        for (Packet<?> packet : packets) {
+            PacketUtil.sendPacket(packet);
+        }
+        packets.clear();
+
+        if (model != null) {
+            model.despawn();
+            model = null;
+        }
+    }
+}
---
+++ src/main/java/me/stormcph/lumina/module/impl/player/GhostPlace.java
@@ -0,0 +1,76 @@
+package me.stormcph.lumina.module.impl.player;
+
+import me.stormcph.lumina.event.EventTarget;
+import me.stormcph.lumina.event.impl.EventUpdate;
+import me.stormcph.lumina.event.impl.PacketSendEvent;
+import me.stormcph.lumina.module.Category;
+import me.stormcph.lumina.module.Module;
+import me.stormcph.lumina.setting.impl.ModeSetting;
+import net.minecraft.block.Blocks;
+import net.minecraft.network.packet.c2s.play.PlayerInteractBlockC2SPacket;
+import net.minecraft.util.math.BlockPos;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GhostPlace extends Module {
+
+    private final ModeSetting mode = new ModeSetting("Mode", "Vanilla", "Vanilla", "Grim");
+    private final List<BlockPos> positions = new ArrayList<>();
+
+    public GhostPlace() {
+        super("Ghostplace", "Every block you place will be a ghost block", Category.PLAYER);
+        addSettings(mode);
+    }
+
+    @Override
+    public void onEnable() {
+        super.onEnable();
+        positions.clear();
+    }
+
+    @EventTarget
+    public void onUpdate(EventUpdate e) {
+
+        if(mode.getMode().equalsIgnoreCase("Vanilla")) {
+            return;
+        }
+
+        for (BlockPos position : positions) {
+            mc.world.setBlockState(position, Blocks.BEDROCK.getDefaultState());
+        }
+    }
+
+    @EventTarget
+    public void onPacket(PacketSendEvent e) {
+        if(e.getPacket() instanceof PlayerInteractBlockC2SPacket pib) {
+
+            if(mode.getMode().equalsIgnoreCase("Vanilla")) {
+                e.cancel();
+            }
+            else {
+
+                BlockPos pos = pib.getBlockHitResult().getBlockPos();
+                switch (pib.getBlockHitResult().getSide()) {
+                    case UP -> pos = pos.up();
+                    case DOWN -> pos = pos.down();
+                    case NORTH -> pos = pos.north();
+                    case SOUTH -> pos = pos.south();
+                    case EAST -> pos = pos.east();
+                    case WEST -> pos = pos.west();
+                }
+
+                positions.add(pos);
+            }
+        }
+    }
+
+    @Override
+    public void onDisable() {
+        super.onDisable();
+        for (BlockPos position : positions) {
+            mc.world.setBlockState(position, Blocks.AIR.getDefaultState());
+        }
+        positions.clear();
+    }
+}
