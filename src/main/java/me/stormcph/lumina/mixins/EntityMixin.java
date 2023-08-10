4d34b5eb8aac309250544c704d0dac0cdf307362
kgurchiek <cornbread2100@cornbread2100.com>
2023-08-10 10:38:06 -0400

    use getModuleByClass

--- src/main/java/me/stormcph/lumina/mixins/EntityMixin.java
+++ src/main/java/me/stormcph/lumina/mixins/EntityMixin.java
@@ -3,6 +3,7 @@ package me.stormcph.lumina.mixins;
 import me.stormcph.lumina.event.impl.PlayerMoveEvent;
 import me.stormcph.lumina.mixinterface.CameraInterface;
 import me.stormcph.lumina.module.ModuleManager;
+import me.stormcph.lumina.module.impl.misc.Freecam;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.network.ClientPlayerEntity;
 import net.minecraft.client.render.Camera;
@@ -27,7 +28,7 @@ public abstract class EntityMixin {
 
     @Inject(method = "move", at = @At("HEAD"), cancellable = true)
     public void onMove(MovementType movementType, Vec3d movement, CallbackInfo ci) {
-        if (this.isPlayer() && ModuleManager.INSTANCE.getModuleByName("Freecam").isEnabled()) ci.cancel();
+        if (this.isPlayer() && ModuleManager.INSTANCE.getModuleByClass(Freecam.class).isEnabled()) ci.cancel();
         if(!(((Object) this) instanceof ClientPlayerEntity)) return;
         if(!(((Object) this) == MinecraftClient.getInstance().player)) return;
 
@@ -40,12 +41,12 @@ public abstract class EntityMixin {
 
     @Inject(method = "pushAwayFrom", at = @At("HEAD"), cancellable = true)
     private void onPushAwayFrom(Entity entity, CallbackInfo ci) {
-        if (this.isPlayer() && ModuleManager.INSTANCE.getModuleByName("Freecam").isEnabled()) ci.cancel();
+        if (this.isPlayer() && ModuleManager.INSTANCE.getModuleByClass(Freecam.class).isEnabled()) ci.cancel();
     }
 
     @Inject(method = "setYaw", at = @At("HEAD"), cancellable = true)
     public void setYaw(float yaw, CallbackInfo ci) {
-        if (this.isPlayer() && ModuleManager.INSTANCE.getModuleByName("Freecam").isEnabled()) {
+        if (this.isPlayer() && ModuleManager.INSTANCE.getModuleByClass(Freecam.class).isEnabled()) {
             Camera camera = mc.gameRenderer.getCamera();
             ((CameraInterface) camera).setFreecamYaw(camera.getYaw() + yaw - this.yaw);
             ci.cancel();
@@ -54,7 +55,7 @@ public abstract class EntityMixin {
 
     @Inject(method = "setPitch", at = @At("HEAD"), cancellable = true)
     public void setPitch(float pitch, CallbackInfo ci) {
-        if (this.isPlayer() && ModuleManager.INSTANCE.getModuleByName("Freecam").isEnabled()) {
+        if (this.isPlayer() && ModuleManager.INSTANCE.getModuleByClass(Freecam.class).isEnabled()) {
             Camera camera = mc.gameRenderer.getCamera();
             ((CameraInterface) camera).setFreecamPitch(camera.getPitch() + pitch - this.pitch);
             ci.cancel();
